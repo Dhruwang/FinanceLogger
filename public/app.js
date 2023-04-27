8fb9257455b06f58b06fdf7972effe1286a9c26a
Dhruwang <dhruwangjariwala18@gmail.com>
2023-04-27 23:31:00 +0530

    completed app.ts

--- public/app.js
+++ public/app.js
@@ -1,20 +1,34 @@
-"use strict";
+import { Invoice } from "./classes/Invoice.js";
+import { ListTemplate } from "./classes/ListTemplate.js";
+import { Payment } from "./classes/Payments.js";
+let docOne;
+let docTwo;
+docOne = new Invoice("Dhruwang", "Bonus", 50000000000000000000);
+docTwo = new Payment("Vw", "CarPayment", 1700000);
+let docs = [];
+docs.push(docOne);
+docs.push(docTwo);
 const anchor = document.querySelector("a");
 // adding an ! mark at the end will tell ts that I know anchor is not null and hence we will not get that error 
 // ts doesnot know whether anchor has a href property or not 
 //here we check whether anchor tag is null or not
-console.log(anchor.href);
 const form = document.querySelector(".new-item-form");
-console.log(form.children);
 // inputs 
 const type = document.querySelector("#type");
 const tofrom = document.querySelector("#tofrom");
 const details = document.querySelector("#details");
 const amount = document.querySelector("#amount");
+const ul = document.querySelector("ul");
+const list = new ListTemplate(ul);
 form.addEventListener("submit", (e) => {
+    console.log("runn9ing");
     e.preventDefault();
-    console.log(type.value);
-    console.log(tofrom.value);
-    console.log(details.value);
-    console.log(amount.value);
+    let doc;
+    if (type.value === "invoice") {
+        doc = new Invoice(tofrom.value, details.value, amount.valueAsNumber);
+    }
+    else {
+        doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
+    }
+    list.render(doc, type.value, "end");
 });
--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,25 @@
+import { Invoice } from "./classes/Invoice.js";
+import { ListTemplate } from "./classes/ListTemplate.js";
+import { Payment } from "./classes/Payments.js";
+import { HasFormatter } from "./Interfaces/HasFormatter.js";
+
+let docOne: HasFormatter;
+let docTwo: HasFormatter;
+
+docOne = new Invoice("Dhruwang","Bonus",50000000000000000000)
+docTwo = new Payment("Vw","CarPayment",1700000)
+
+let docs:HasFormatter[]=[]
+docs.push(docOne)
+docs.push(docTwo)
+
 const anchor = document.querySelector("a")!;
 // adding an ! mark at the end will tell ts that I know anchor is not null and hence we will not get that error 
 
 // ts doesnot know whether anchor has a href property or not 
 //here we check whether anchor tag is null or not
-console.log(anchor.href)
 
 const form = document.querySelector(".new-item-form") as HTMLFormElement;
-console.log(form.children)
 
 // inputs 
 const type = document.querySelector("#type") as HTMLSelectElement;
@@ -14,10 +27,18 @@ const tofrom = document.querySelector("#tofrom") as HTMLInputElement;
 const details = document.querySelector("#details") as HTMLInputElement;
 const amount = document.querySelector("#amount") as HTMLInputElement;
 
-form.addEventListener("submit",(e:Event)=>{
-    e.preventDefault()
-    console.log(type.value)
-    console.log(tofrom.value)
-    console.log(details.value)
-    console.log(amount.value)
+const ul = document.querySelector("ul") !;
+const list = new ListTemplate(ul)
+
+form.addEventListener("submit",(e)=>{
+    console.log("runn9ing")
+    e.preventDefault();
+    let doc:HasFormatter;
+    if (type.value ==="invoice") {
+        doc = new Invoice(tofrom.value, details.value, amount.valueAsNumber)
+    }
+    else{
+        doc = new Payment(tofrom.value, details.value, amount.valueAsNumber)
+    }
+    list.render(doc, type.value, "end")
 })
